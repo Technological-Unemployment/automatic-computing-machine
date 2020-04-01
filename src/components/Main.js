8cf213187d8d702e72b7292d8891e58276c3bf1c
Technological-Unemployment <josephjones7@fastmail.fm>
2020-04-01 07:53:12 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -111,9 +111,10 @@ class Main extends React.Component {
           id="contact"
           className={`${this.props.article === 'contact' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
+          data-netlify="true" data-netlify-honeypot="bot-field"
           }`}
+          <input type="hidden" name="bot-field" />  
+          <input type="hidden" name="form-name" value="contact" />
           style={{ display: 'none' }}
           
         >
