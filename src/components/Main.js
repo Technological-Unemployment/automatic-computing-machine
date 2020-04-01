e58febe577aa78f7907966ec9290e806f58c3cdd
Technological-Unemployment <josephjones7@fastmail.fm>
2020-04-01 08:24:32 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -116,7 +116,7 @@ class Main extends React.Component {
           
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
